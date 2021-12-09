d738eaac697d52df7c6f16bbf300ae45b8213190
Peter Park <mac-main@Peters-MBP-2.lan>
2021-12-09 14:16:30 -0800

    Initial Repo Setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: __dirname + '/client/src/app.jsx',
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+}
