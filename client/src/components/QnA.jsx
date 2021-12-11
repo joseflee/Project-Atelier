de4687e6e89bb8f04e2118c1d0b912446517fc8e
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-10 18:04:04 -0800

    Added folder for QnA components, refactored QnA to class component

--- client/src/components/QnA.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const QnA = (props) => (
-<div>This is my component</div>
-)
-
-export default QnA;
---
+++ client/src/components/QnAcomponent/QnA.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class QnA extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>This is my class component</div>
+    )
+  }
+}
+
+export default QnA;
