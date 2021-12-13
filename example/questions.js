707f40f1b61a3eb8d61e6c95b56f91763ff41327
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-13 12:13:49 -0800

    Added example data - questions list

---
+++ example/questions.js
@@ -0,0 +1,57 @@
+/*GET /qa/questions Retrieves a list of questions for a particular product. This list does not include any reported questions.
+Params:
+- product_id (int) - Specifies the product for which to retrieve questions.
+- page (int) - Selects the page of results to return. Default 1.
+- count (int) - Specifies how many results per page to return. Default 5.
+Response:
+Status: 200 OK
+*/
+
+{
+  "product_id": "5",
+  "results": [{
+        "question_id": 37,
+        "question_body": "Why is this product cheaper here than other sites?",
+        "question_date": "2018-10-18T00:00:00.000Z",
+        "asker_name": "williamsmith",
+        "question_helpfulness": 4,
+        "reported": false,
+        "answers": {
+          68: {
+            "id": 68,
+            "body": "We are selling it here without any markup from the middleman!",
+            "date": "2018-08-18T00:00:00.000Z",
+            "answerer_name": "Seller",
+            "helpfulness": 4,
+            "photos": []
+          }
+        }
+      },
+      {
+        "question_id": 38,
+        "question_body": "How long does it last?",
+        "question_date": "2019-06-28T00:00:00.000Z",
+        "asker_name": "funnygirl",
+        "question_helpfulness": 2,
+        "reported": false,
+        "answers": {
+          70: {
+            "id": 70,
+            "body": "Some of the seams started splitting the first time I wore it!",
+            "date": "2019-11-28T00:00:00.000Z",
+            "answerer_name": "sillyguy",
+            "helpfulness": 6,
+            "photos": [],
+          },
+          78: {
+            "id": 78,
+            "body": "9 lives",
+            "date": "2019-11-12T00:00:00.000Z",
+            "answerer_name": "iluvdogz",
+            "helpfulness": 31,
+            "photos": [],
+          }
+        }
+      }
+  ]
+}
