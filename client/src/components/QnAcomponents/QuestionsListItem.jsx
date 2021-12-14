2cf1c9ee4cbfb161a9ada0708858dac710fd2d42
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-14 12:01:54 -0800

    Refactored QuestionListItem as a class component

--- client/src/components/QnAcomponents/QuestionsListItem.jsx
+++ client/src/components/QnAcomponents/QuestionsListItem.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import QuestionsListItemAnswer from './QuestionsListItemAnswer.jsx';
 
-const QuestionsListItem = (props) => (
+class QuestionsListItem extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
-<div>
+  render(){
+    return(
+      <div>
 
-  {/* beginning of question item */}
+        {/* beginning of question item */}
 
-  <div className='question-item'>
-    <div className='question-item-q-letter'><h2>Q:</h2></div><div className='question-item-body'><h3>{props.question.question_body}</h3></div>
-    <div className='question-item-helpful-keyword'>Helpful?</div>
-    <div className='question-item-yes-button'>Yes({props.question.question_helpfulness})</div>
-    <div className='question-item-add-answer-link'>Add answer</div>
-  </div>
- {/* end of question item */}
- {
-   Object.values(props.question.answers).map((answer, key) => {
-     return <QuestionsListItemAnswer answer={answer} key={key}/>
-   })
- }
+        <div className='question-item'>
+          <div className='question-item-q-letter'><h2>Q:</h2></div><div className='question-item-body'><h3>{this.props.question.question_body}</h3></div>
+          <div className='question-item-helpful-keyword'>Helpful?</div>
+          <div className='question-item-yes-button'>Yes({this.props.question.question_helpfulness})</div>
+          <div className='question-item-add-answer-link'>Add answer</div>
+        </div>
+       {/* end of question item */}
+       {
+         Object.values(this.props.question.answers).map((answer, key) => {
+           return <QuestionsListItemAnswer answer={answer} key={key}/>
+         })
+       }
 
-</div>
-)
+      </div>
+      )
+
+
+  }
+
+
+
+
+}
 
 export default QuestionsListItem;
