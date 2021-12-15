3194a1e0ddd6918ff89918c5925cbf37458d4699
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-14 17:24:59 -0800

    Component 'MoreAnsweredQuestions' is rendered conditionally

--- client/src/components/QnAcomponents/mainQnA.jsx
+++ client/src/components/QnAcomponents/mainQnA.jsx
@@ -8,15 +8,33 @@ import sampleData from '../../../../example/questions.js';
 class QnA extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      questions: sampleData.questions.results
+    }
+  }
+
+  componentDidMount(){
+    var questionsToShow = sampleData.questions.results;
+    questionsToShow = questionsToShow.slice(0, 2);
+    this.setState({
+      questions: questionsToShow
+    })
   }
 
   render(){
+    let moreAnsweredQuestions;
+    if(this.state.questions.length > 2){
+      moreAnsweredQuestions = <MoreAnsweredQuestions />
+    } else {
+      moreAnsweredQuestions = <div></div>
+    }
     return(
       <div>
+        {console.log(sampleData.questions.results)}
       <div className='qna-component-name'>Questions and Answers</div>
       <SearchQuestions />
-      <QuestionsList data={sampleData}/>
-      <MoreAnsweredQuestions />
+      <QuestionsList data={this.state.questions}/>
+      {moreAnsweredQuestions}
       <AddQuestion />
       </div>
 
