8f88b16570fb4dba241d7d905165fb75e8487a8c
a244629128 <gjyyl9394967@gmail.com>
2021-12-16 16:25:19 -0500

    fix syntax error

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProductOverview from './components/ProdOverview/OverView.jsx'
-import RelProducts from './components/RelProductsComponents/RelProducts.jsx'
+import ProductOverview from './components/ProdOverview/OverView.jsx';
+import RelProducts from './components/RelProductsComponents/RelProducts.jsx';
 import QnA from './components/QnAcomponents/mainQnA.jsx';
-import RatingsNReviews from './components/RatingsNReviews/RatingsNReviews.jsx'
+import RatingsNReviews from './components/RatingsNReviews/RatingsNReviews.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
