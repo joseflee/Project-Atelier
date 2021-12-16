e422e8925980612ff7a74b1958fa3e00716ff793
a244629128 <gjyyl9394967@gmail.com>
2021-12-15 20:40:20 -0500

    minor change on API call

--- server/RatingApi.js
+++ server/RatingApi.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 const gitToken = require('../config.js');
 
 
-const getTotalReviews = (productId) => {
+const getTotalReviews = (productId, page) => {
    productId = 59553;
   let options = {
     method: 'GET',
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews?product_id=${productId}&count=100&sort=newest&page=1`,
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews?product_id=${productId}&count=50&sort=newest&page=${page}`,
     headers: { Authorization: gitToken.Token },
   };
   return axios(options)
