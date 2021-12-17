5be78b9d5896be32a9f63c6300e15dddb103ee89
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-16 19:43:41 -0800

    Added 3 input fields

--- client/src/components/QnAcomponents/AddAnswerForm.jsx
+++ client/src/components/QnAcomponents/AddAnswerForm.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 class AddAnswerForm extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      answerBody:'',
+      nickname:'',
+      email:''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event){
+
+  }
+
+  handleSubmit(){
 
   }
 
@@ -11,6 +25,48 @@ class AddAnswerForm extends React.Component {
       <div>
         <div>Submit your answer</div>
         <div>{this.props.name}: {this.props.question_body}</div>
+        <form>
+            <label>
+              Your answer*
+              <input
+                name='answerBody'
+                type='text'
+                maxlength='1000'
+                value={this.state.answerBody}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <br />
+            <label>
+              What's your nickname?*
+              <input
+                name='nickname'
+                type='text'
+                maxlength='60'
+                placeholder='Example: jack543'
+                value={this.state.nickname}
+                onChange={this.handleInputChange}
+              />
+              <br />
+              <i>For privacy reasons, do not use your full name or email address</i>
+            </label>
+            <br />
+            <label>
+              Your email?*
+              <input
+                name='email'
+                type='text'
+                maxlength='60'
+                placeholder='Example: jack@email.com'
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+              <br />
+              <i>For authentication reasons, you will not be emailed</i>
+            </label>
+            <br />
+            <input type="submit" value="Submit" onClick = {(e)=>this.handleSubmit(e)} />
+          </form>
       </div>
     )
   }
