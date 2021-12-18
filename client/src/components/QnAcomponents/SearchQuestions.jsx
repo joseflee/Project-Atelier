e504367f04520ec5784163cd14d4ffc0537b2297
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-17 18:20:18 -0800

    Added form with input field and handleInputChange function

--- client/src/components/QnAcomponents/SearchQuestions.jsx
+++ client/src/components/QnAcomponents/SearchQuestions.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 class SearchQuestions extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange() {
+    
   }
 
   render() {
     return (
-      <div>Search here</div>
+      <form>
+        <input
+          type='text'
+          placeholder='Have a question? Search for answers…'
+          size='50'
+          onChange={this.handleInputChange} />
+      </form>
     );
   }
 
