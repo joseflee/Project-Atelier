1a02d024034a2b496f8d4554b67608bbdb731d1b
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-18 15:15:18 -0800

    Fixed syntax errors

--- client/src/components/QnAcomponents/mainQnA.jsx
+++ client/src/components/QnAcomponents/mainQnA.jsx
@@ -6,85 +6,85 @@ import AddQuestion from './AddQuestion.jsx';
 import sampleData from '../../../../example/questions.js';
 
 class QnA extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       questions: [],
       isMoreQuestionsButtonShown: false,
-      product_name:'Blue jeans'
-    }
+      productName: 'Blue jeans'
+    };
     this.showMoreQuestions = this.showMoreQuestions.bind(this);
     this.search = this.search.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     var questionsToShow = sampleData.questions.results;
-    if (questionsToShow.length > 2){
+    if (questionsToShow.length > 2) {
       this.setState({
         isMoreQuestionsButtonShown: true
-      })
+      });
     }
     questionsToShow = questionsToShow.slice(0, 2);
     this.setState({
       questions: questionsToShow
-    })
+    });
   }
 
-  showMoreQuestions(){
+  showMoreQuestions() {
     this.setState({
       questions: sampleData.questions.results,
       isMoreQuestionsButtonShown: false
-    })
+    });
   }
 
-  search(query, isSearchTriggered){
-    if(isSearchTriggered){
+  search(query, isSearchTriggered) {
+    if (isSearchTriggered) {
       query = query.toLowerCase();
       console.log('received query', query);
       //console.log(this.state.questions);
-      let questions =  sampleData.questions.results;
+      let questions = sampleData.questions.results;
       const filtered = questions.filter(item => item.question_body.toLowerCase().includes(query));
       console.log('filtered', filtered);
-     //do not hiding questions if more than 2
-     this.setState({
-       questions: filtered
-     })
+      //do not hiding questions if more than 2
+      this.setState({
+        questions: filtered
+      });
     } else {
       console.log('search stopped');
       //render all the questions and hide the rest if more than 2
       var questionsToShow = sampleData.questions.results;
-      if (questionsToShow.length > 2){
+      if (questionsToShow.length > 2) {
         this.setState({
           isMoreQuestionsButtonShown: true
-        })
+        });
       }
       questionsToShow = questionsToShow.slice(0, 2);
       this.setState({
         questions: questionsToShow
-      })
+      });
     }
 
   }
 
-  render(){
+  render() {
     let moreAnsweredQuestions;
-    if(this.state.isMoreQuestionsButtonShown){
-      moreAnsweredQuestions = <MoreAnsweredQuestions click={this.showMoreQuestions}/>
+    if (this.state.isMoreQuestionsButtonShown) {
+      moreAnsweredQuestions = <MoreAnsweredQuestions click={this.showMoreQuestions}/>;
     } else {
-      moreAnsweredQuestions = <div></div>
+      moreAnsweredQuestions = <div></div>;
     }
-    return(
+    return (
       <div>
         {console.log(sampleData.questions.results)}
-      <div className='qna-component-name'>Questions and Answers</div>
-      <SearchQuestions search={this.search}/>
-      <QuestionsList data={this.state.questions}/>
-      <br />
-      {moreAnsweredQuestions}
-      <AddQuestion name = {this.state.product_name}/>
+        <div className='qna-component-name'>Questions and Answers</div>
+        <SearchQuestions search={this.search}/>
+        <QuestionsList data={this.state.questions}/>
+        <br />
+        {moreAnsweredQuestions}
+        <AddQuestion name = {this.state.product_name}/>
       </div>
 
-    )
+    );
   }
 }
 
