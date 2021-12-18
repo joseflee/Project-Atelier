e27a49b0a08e896272961f2d52bb0fbe6967db8f
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-17 19:24:12 -0800

    Filtered questions array by search query

--- client/src/components/QnAcomponents/mainQnA.jsx
+++ client/src/components/QnAcomponents/mainQnA.jsx
@@ -39,6 +39,11 @@ class QnA extends React.Component {
 
   search(query){
     console.log('received query', query);
+    console.log(this.state.questions);
+    let questions = [...this.state.questions];
+    const filtered = questions.filter(item => item.question_body.includes(query));
+    console.log('filtered', filtered);
+    
   }
 
   render(){
