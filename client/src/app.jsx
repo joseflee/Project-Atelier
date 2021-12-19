68d67a6f5d927c8a7f79fd36c0f15fdf9968d94e
ashtsai14 <atsai14@gmail.com>
2021-12-19 14:36:26 -0800

    Resolved app.js conflicts

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import ProductOverview from './components/ProdOverview/OverView.jsx';
 import RelProducts from './components/RelProductsComponents/RelProducts.jsx';
 import QnA from './components/QnAcomponents/mainQnA.jsx';
 import RatingsNReviews from './components/RatingsNReviews/RatingsNReviews.jsx';
 
-=======
-import ProductOverview from './components/ProdOverview/OverView.jsx'
-// import RelProducts from './components/RelProductsComponents/RelProducts.jsx'
-// import QnA from './components/QnAcomponents/mainQnA.jsx';
-// import RatingsNReviews from './components/RatingsNReviews/RatingsNReviews.jsx'
->>>>>>> 960e260 (Implemented a main image and -click to display- thumbnail gallery)
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -28,15 +21,9 @@ class App extends React.Component {
     return (
       <div>
         <ProductOverview />
-<<<<<<< HEAD
         <RelProducts />
         <QnA productId={this.state.productId} />
         <RatingsNReviews productId={this.state.productId} />
-=======
-        {/* <RelProducts />
-        <QnA />
-        <RatingsNReviews /> */}
->>>>>>> 960e260 (Implemented a main image and -click to display- thumbnail gallery)
       </div>
     );
   }
