15d05fab0be673a2e86a5a00a63e9f68cce2aaea
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-19 11:26:56 -0800

    search() connected with api response

--- client/src/components/QnAcomponents/mainQnA.jsx
+++ client/src/components/QnAcomponents/mainQnA.jsx
@@ -16,7 +16,6 @@ class QnA extends React.Component {
     };
     this.showMoreQuestions = this.showMoreQuestions.bind(this);
     this.search = this.search.bind(this);
-    //this.getQuestionsList = this.getQuestionsList.bind(this);
   }
 
   componentDidMount() {
@@ -69,31 +68,40 @@ class QnA extends React.Component {
   }
 
   search(query, isSearchTriggered) {
-    if (isSearchTriggered) {
-      query = query.toLowerCase();
-      console.log('received query', query);
-      //console.log(this.state.questions);
-      let questions = sampleData.questions.results;
-      const filtered = questions.filter(item => item.question_body.toLowerCase().includes(query));
-      //console.log('filtered', filtered);
-      //do not hiding questions if more than 2
-      this.setState({
-        questions: filtered
+    let productId = this.props.productId;
+
+    //GET LIST OF ALL QUESTIONS BY PRODUCT ID
+    var url = 'http://localhost:3000/qna/getQuestionsList';
+    axios.get(url, {params: {id: productId}})
+      .then((response) => {
+        if (isSearchTriggered) {
+          query = query.toLowerCase();
+          console.log('received query', query);
+          let questions = response.data.results;
+          const filtered = questions.filter(item => item.question_body.toLowerCase().includes(query));
+          console.log('filtered', filtered);
+          //do not hiding questions if more than 2
+          this.setState({
+            questions: filtered
+          });
+        } else {
+          console.log('search stopped');
+          //render all the questions and hide the rest if more than 2
+          var questionsToShow = response.data.results;
+          if (questionsToShow.length > 2) {
+            this.setState({
+              isMoreQuestionsButtonShown: true
+            });
+          }
+          questionsToShow = questionsToShow.slice(0, 2);
+          this.setState({
+            questions: questionsToShow
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    } else {
-      //console.log('search stopped');
-      //render all the questions and hide the rest if more than 2
-      var questionsToShow = sampleData.questions.results;
-      if (questionsToShow.length > 2) {
-        this.setState({
-          isMoreQuestionsButtonShown: true
-        });
-      }
-      questionsToShow = questionsToShow.slice(0, 2);
-      this.setState({
-        questions: questionsToShow
-      });
-    }
   }
 
   render() {
@@ -105,8 +113,7 @@ class QnA extends React.Component {
     }
     return (
       <div>
-        {/* {console.log(sampleData.questions.results)} */}
-        <div className='qna-component-name'>Questions and Answers</div>
+        <div className='qna-component-name'><h1>Questions and Answers</h1></div>
         <SearchQuestions search={this.search}/>
         <QuestionsList data={this.state.questions}/>
         <br />
