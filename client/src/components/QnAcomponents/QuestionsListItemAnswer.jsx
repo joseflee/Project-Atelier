4a47695d2fa40b5b5b1abc40230ee979f31e625b
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-19 15:41:33 -0800

    Rewrited as a class component

--- client/src/components/QnAcomponents/QuestionsListItemAnswer.jsx
+++ client/src/components/QnAcomponents/QuestionsListItemAnswer.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const QuestionsListItemAnswer = (props) => (
-  <div className='answer-item'>
-    <div className='answer-item-a-letter'><h2>A:{props.answer.body}</h2></div>
-    {/* <div className='answer-item-body'><h3></h3></div> */}
-    <div className='answer-item-username'>By user {props.answer.answerer_name}</div><div className='answer-item-date'>{props.answer.date}</div>
-    <div className='answer-item-photos'></div>
-    <div className='answer-item-helpful-keyword'>Helpful?</div>
-    <div className='answer-item-yes-button'>Yes({props.answer.helpfulness})</div>
-    <div className='answer-item-report-button'>Report</div>
-  </div>
-);
+// const QuestionsListItemAnswer = (props) => (
+//
+// );
+
+class QuestionsListItemAnswer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='answer-item'>
+        <div className='answer-item-a-letter'><h2>A:{this.props.answer.body}</h2></div>
+        <div className='answer-item-username'>By user {this.props.answer.answerer_name}</div><div className='answer-item-date'>{this.props.answer.date}</div>
+        <div className='answer-item-photos'></div>
+        <div className='answer-item-helpful-keyword'>Helpful?</div>
+        <div className='answer-item-yes-button'>Yes({this.props.answer.helpfulness})</div>
+        <div className='answer-item-report-button'>Report</div>
+      </div>
+    );
+  }
+}
 
 export default QuestionsListItemAnswer;
