d84f7c71ae9d54dd0d44311e6c306e5770ba9a34
Peter Park <mac-main@Peters-MBP-2.lan>
2021-12-19 18:09:53 -0800

    Created if statements for if no 'my outfits' are present, nothing is rendered

--- client/src/components/RelProductsComponents/MyOutfitCards.jsx
+++ client/src/components/RelProductsComponents/MyOutfitCards.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
 var MyOutfitCards = (props) => {
-  return (
-    <div>
-      <h3>My Outfits Cards</h3>
-      {props.myOutfitCards.map(outfit => {
-        return (
-          <div key={outfit.id}>
-            <h4 className="category-myOutfits">{outfit.category}</h4>
-            <h4 className="name-myOutfits">{outfit.name}</h4>
-            <h4 className="price-myOutfits">{outfit.default_price}</h4>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (props.myOutfitCards === null) {
+    return (
+      <></>
+    );
+  } else {
+    return (
+      <div>
+        <h3>My Outfits Cards</h3>
+        {props.myOutfitCards.map(outfit => {
+          return (
+            <div key={outfit.id}>
+              <h4 className="category-myOutfits">{outfit.category}</h4>
+              <h4 className="name-myOutfits">{outfit.name}</h4>
+              <h4 className="price-myOutfits">{outfit.default_price}</h4>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 export default MyOutfitCards;
