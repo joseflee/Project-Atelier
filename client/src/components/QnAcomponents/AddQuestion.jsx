28ac74e0d42936b7c9ccc8760cb848785b2a1adc
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-20 12:17:05 -0800

    Fixed syntax problems

--- client/src/components/QnAcomponents/AddQuestion.jsx
+++ client/src/components/QnAcomponents/AddQuestion.jsx
@@ -2,43 +2,43 @@ import React from 'react';
 import AddQuestionForm from './AddQuestionForm.jsx';
 
 class AddQuestion extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isModalShown: false,
       isAddButtonShown: true,
       product_id: 1
-    }
+    };
     this.clickOnAddQuestion = this.clickOnAddQuestion.bind(this);
   }
 
-  clickOnAddQuestion(){
+  clickOnAddQuestion() {
     this.setState({
       isModalShown: true,
       isAddButtonShown: false
-    })
+    });
   }
 
 
-  render(){
-    let modal;
-    let questionButton;
-    if(this.state.isModalShown){
-      modal = <div><AddQuestionForm name={this.props.name}/></div>
+  render() {
+    let modal,
+      questionButton;
+    if (this.state.isModalShown) {
+      modal = <div><AddQuestionForm name={this.props.name}/></div>;
     } else {
-      modal = <div></div>
+      modal = <div></div>;
     }
-    if(this.state.isAddButtonShown){
-      questionButton =  <button onClick={()=>this.clickOnAddQuestion()}>Add a new question</button>
+    if (this.state.isAddButtonShown) {
+      questionButton = <button onClick={()=>this.clickOnAddQuestion()}>Add a new question</button>;
     } else {
-      questionButton = <div></div>
+      questionButton = <div></div>;
     }
-    return(
+    return (
       <div>
         {questionButton}
         {modal}
       </div>
-    )
+    );
   }
 }
 
