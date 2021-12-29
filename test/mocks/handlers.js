16b2313736f436da34c260800a8ad49cf2daa5c6
a244629128 <gjyyl9394967@gmail.com>
2021-12-28 22:33:39 -0500

    some rendering tests and fix memory leak error

--- test/mocks/handlers.js
+++ test/mocks/handlers.js
@@ -1,9 +1,8 @@
 import { rest } from 'msw';
-
+import {dummyReviews} from '../../example/reviews.js';
 // Create your own js file with these handlers or mock server responses containing example data within this mocks folder if needed
 
 // Looks very similar to regular server expressions, but 'ctx' is used to handle response handlers instead of res.
-
 export const handlers = [
   rest.get('/product', (req, res, ctx) => {
     return res(
@@ -14,9 +13,16 @@ export const handlers = [
     );
   }),
 
-  rest.get('/ratings', (req, res, ctx) => {
+  rest.get('/ratings/getReviews', (req, res, ctx) => {
     return res(
-      ctx.status(200)
+      ctx.status(200),
+      ctx.json(dummyReviews.results)
+    );
+  }),
+  rest.get('/ratings/ratingOverview', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(dummyReviews.meta)
     );
   }),
 
