d3e7a526427ee6c5d3a583d11419308a590d8eb2
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-01-03 12:45:39 -0800

    Created and added empty AnswerPhotoUpload component

---
+++ client/src/components/QnAcomponents/AnswerPhotoUpload.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhotoUpload = (props) => {
+  return (
+    <div>This is photo upload component</div>
+  );
+};
+
+export default PhotoUpload;
--- client/src/components/QnAcomponents/QuestionsListItemAnswer.jsx
+++ client/src/components/QnAcomponents/QuestionsListItemAnswer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import moment from 'moment';
+import AnswerPhotoUpload from './AnswerPhotoUpload.jsx';
 
 class QuestionsListItemAnswer extends React.Component {
   constructor(props) {
@@ -56,6 +57,7 @@ class QuestionsListItemAnswer extends React.Component {
     return (
       <div className='answer-item'>
         <div className='answer-item-a-letter'><h2>A:{this.props.answer.body}</h2></div>
+        <div className='answer-photo-upload'><AnswerPhotoUpload /></div>
         <div className='answer-item-username'>By user {this.props.answer.answerer_name}</div><div className='answer-item-date'>{moment(this.props.answer.date).format('MMMM DD, YYYY')};</div>
         <div className='answer-item-photos'></div>
         <div className='answer-item-helpful-keyword'>Helpful?</div>
