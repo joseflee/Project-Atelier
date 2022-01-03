dacee8476ef2d445d7693f5bc496f3cd40fd6dd9
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-01-03 15:21:59 -0800

    Refactored photo upload as a class component

--- client/src/components/QnAcomponents/AnswerPhotoUpload.jsx
+++ client/src/components/QnAcomponents/AnswerPhotoUpload.jsx
@@ -1,23 +1,41 @@
 import React, {useState} from 'react';
 
-const PhotoUpload = (props) => {
-  const [file, setFile] = useState([]);
+class PhotoUpload extends React.Component {
+  //const [file, setFile] = useState([]);
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: []
+    };
 
-  let uploadSingleFile = function uploadSingleFile(e) {
-    setFile([...file, URL.createObjectURL(e.target.files[0])]);
-  };
+    this.uploadSingleFile = this.uploadSingleFile.bind(this);
 
-  let upload = function upload(e) {
-    e.preventDefault();
-    console.log('clicked on upload');
-    props.handlePhotos(file);
-  };
+  }
 
-  return (
-    <div>
-      <div className="form-group preview">
-        {file.length > 0 &&
-          file.map((item, index) => {
+
+  uploadSingleFile(e) {
+    // setFile([...file, URL.createObjectURL(e.target.files[0])], ()=> {
+    //   props.handlePhotos(file);
+    // });
+    this.setState({
+      file: [...this.state.file, URL.createObjectURL(e.target.files[0])]
+    }, ()=> {
+      this.props.handlePhotos(this.state.file);
+    });
+
+  }
+
+  // let upload = function upload(e) {
+  //   e.preventDefault();
+  //   console.log('clicked on upload');
+  //   props.handlePhotos(file);
+  // };
+  render() {
+    return (
+      <div>
+        <div className="form-group preview">
+          {this.state.file.length > 0 &&
+          this.state.file.map((item, index) => {
             return (
               <div key={item}>
                 <img src={item} alt="" width={'350px'}/>
@@ -27,25 +45,20 @@ const PhotoUpload = (props) => {
               </div>
             );
           })}
-      </div>
+        </div>
 
-      <div className="form-group">
-        <input
-          type="file"
-          disabled={file.length === 5}
-          className="form-control"
-          onChange={uploadSingleFile}
-        />
+        <div className="form-group">
+          <input
+            type="file"
+            disabled={this.state.file.length === 5}
+            className="form-control"
+            onChange={this.uploadSingleFile}
+          />
+        </div>
+
       </div>
-      <button
-        type="button"
-        className="btn btn-primary btn-block"
-        onClick={upload}
-      >
-        Upload photos
-      </button>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PhotoUpload;
