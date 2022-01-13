71d2cd3f5b824f96387ffef8a83682c79cdb3a79
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-01-12 19:36:20 -0800

    Refactored rendering tests in main qna

--- test/components/QnASpec/mainQnA.test.js
+++ test/components/QnASpec/mainQnA.test.js
@@ -12,20 +12,20 @@ import MainQnA from '../../../client/src/components/QnAcomponents/mainQnA.jsx';
 
 
 describe('Main Questions and Answers form', function() {
-  xit('should render without throwing an error', function() {
-    expect(shallow(<MainQnA productId={59553}/>).contains( <div className='qna-component-name'><h1>Questions and Answers</h1></div>)).toBe(true);
+  it('should render without throwing an error', function() {
+    expect(shallow(<MainQnA productId={59553}/>).contains( <div className='qna-component-name'>QUESTIONS AND ANSWERS</div>)).toBe(true);
   });
 
-  xit('should be selectable by class', function() {
+  it('should be selectable by class', function() {
     expect(shallow(<MainQnA productId={59553} />).is('.qna-main-component')).toBe(true);
   });
 
-  xit('should mount in a full DOM', function() {
-    expect(mount(<MainQnA productId={59553}/>).find('.qna-main-component').length).toBe(1);
+  it('should mount in a full DOM', function() {
+    expect(shallow(<MainQnA productId={59553}/>).find('.qna-main-component').length).toBe(1);
   });
 
-  xit('should render to static HTML', function() {
-    var text = 'Questions and AnswersAdd a new question';
+  it('should render to static HTML', function() {
+    var text = 'QUESTIONS AND ANSWERSADD A QUESTIONAsk a question about the This is not a nameYour question*What\'s your nickname?*For privacy reasons, do not use your full name or email addressYour email?*For authentication reasons, you will not be emailedClose without submitting';
     expect(render(<MainQnA productId={59553}/>).text()).toEqual(text);
   });
 
