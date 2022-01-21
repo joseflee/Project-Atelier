49992e95c661234ba143826b79751a6cb6903146
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-01-20 19:13:45 -0800

    Added array with only 1 question

--- example/questions.js
+++ example/questions.js
@@ -85,6 +85,47 @@ let questions = {
   ]
 };
 
+let questionsShort = {
+  'product_id': '42',
+  'results': [
+    {
+      'question_id': 38,
+      'question_body': 'How long does it last?',
+      'question_date': '2019-06-28T00:00:00.000Z',
+      'asker_name': 'funnygirl',
+      'question_helpfulness': 2,
+      'reported': false,
+      'answers': {
+        70: {
+          'id': 70,
+          'body': 'Some of the seams started splitting the first time I wore it!',
+          'date': '2019-11-28T00:00:00.000Z',
+          'answerer_name': 'sillyguy',
+          'helpfulness': 6,
+          'photos': [],
+        },
+        78: {
+          'id': 78,
+          'body': '9 lives',
+          'date': '2019-11-12T00:00:00.000Z',
+          'answerer_name': 'iluvdogz',
+          'helpfulness': 31,
+          'photos': []
+        },
+        79: {
+          'id': 79,
+          'body': '42 months',
+          'date': '2019-11-12T00:00:00.000Z',
+          'answerer_name': 'dolphin',
+          'helpfulness': 99,
+          'photos': []
+        }
+      }
+    },
+
+  ]
+};
+
 /*
 Answers list
 
@@ -193,5 +234,6 @@ Status: 204 NO CONTENT
 
 module.exports = {
   questions,
-  answers
+  answers,
+  questionsShort
 };
